dbfc2581d91e20ba4d3f7db8e4c152fc1af3886b
Ferrero Emanuele <emanuele.ferrero@fabrick.com>
2025-01-09 00:25:05 +0100

    filter-box component generated

---
+++ src/app/components/filter-box/filter-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-box',
+  imports: [],
+  templateUrl: './filter-box.component.html',
+  styleUrl: './filter-box.component.sass'
+})
+export class FilterBoxComponent {
+
+}

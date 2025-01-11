d92f5403a444e6029e1ef4dbfc33b5ff7c33efda
Ferrero Emanuele <emanuele.ferrero93@gmail.com>
2025-01-11 01:23:15 +0100

    changes in organization stored data

--- src/app/components/trips-list/trips-list.component.ts
+++ src/app/components/trips-list/trips-list.component.ts
@@ -1,24 +1,12 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, output } from '@angular/core';
 import { AllTripsResponse, Trip } from '../../dto/trips';
 import { TripsService } from '../../services/trips.service';
 import { Observable, Subject, takeUntil, tap } from 'rxjs';
 import { MatTableModule } from '@angular/material/table';
-import { Router } from '@angular/router';
-import { MatPaginatorDefaultOptions, MatPaginatorModule, MatPaginatorSelectConfig, PageEvent } from '@angular/material/paginator';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { TripCardComponent } from './trip-card/trip-card.component';
 import { TripsStore } from '../../stores/trips.store';
-
-// #region Type aliases (1)
-
-type PaginationConfig = {
-  page: number;
-  pageSize: number;
-  total: number;
-};
-
-// #endregion Type aliases (1)
-
-// #region Classes (1)
+import { PaginationObj } from '../../dto/store';
 
 @Component({
   selector: 'app-trips-list',
@@ -30,10 +18,8 @@ type PaginationConfig = {
   templateUrl: './trips-list.component.html',
   styleUrl: './trips-list.component.scss'
 })
-export class TripsListComponent implements OnInit, OnDestroy {
-  // #region Properties (5)
-
-  private unsubscribe = new Subject<void>();
+export class TripsListComponent implements OnInit {
+  // #region Properties (4)
 
   public displayedColumns = [
     'thumbnail',
@@ -42,66 +28,30 @@ export class TripsListComponent implements OnInit, OnDestroy {
     'nrOfRatings',
     'action'
   ];
-  public paginationConfig: PaginationConfig;
+  public refreshList = output();
   public store = inject(TripsStore);
   public tripsList: Trip[] = [];
 
-  // #endregion Properties (5)
+  // #endregion Properties (4)
 
   // #region Constructors (1)
 
-  constructor(
-    private readonly tripService: TripsService,
-    private readonly router: Router
-  ) {
-    this.paginationConfig = {
-      page: 0,
-      total: 0,
-      pageSize: 10
-    };
-  }
+  constructor() { }
 
   // #endregion Constructors (1)
 
-  // #region Public Methods (3)
+  // #region Public Methods (2)
 
   public handlePageChange(event: PageEvent): void {
-    // this.paginationConfig.page = event.pageIndex;
     this.store.updatePagination({ ...this.store.pagination(), page: event.pageIndex + 1 });
-    this.getTrips().subscribe();
-  }
-
-  public ngOnDestroy(): void {
-    this.unsubscribe.next();
-    this.unsubscribe.complete();
+    this.refreshList.emit();
   }
 
   public ngOnInit(): void {
     if (this.store.trips().length === 0) {
-      this.getTrips().subscribe();
+      this.refreshList.emit();
     }
   }
 
-  // #endregion Public Methods (3)
-
-  // #region Private Methods (1)
-
-  private getTrips(): Observable<AllTripsResponse> {
-    return this.tripService.getTrips().pipe(
-      takeUntil(this.unsubscribe),
-      tap(data => {
-        this.store.updateTrips(data.items.map(item => this.tripService.convertTripFromBeToFe(item)));
-        // this.tripsList = data.items.map(item => this.tripService.convertTripFromBeToFe(item));
-        this.store.updatePagination({
-          pageSize: data.limit,
-          total: data.total,
-          page: data.page
-        });
-      })
-    );
-  }
-
-  // #endregion Private Methods (1)
+  // #endregion Public Methods (2)
 }
-
-// #endregion Classes (1)

e6abf80e8e7171d4401ca093a129086821c320c7
Ferrero Emanuele <emanuele.ferrero93@gmail.com>
2025-01-14 18:26:55 +0100

    home tests

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { TripsListComponent } from '../trips-list/trips-list.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { SortingBoxComponent } from '../sorting-box/sorting-box.component';
 import { FilterBoxComponent } from '../filter-box/filter-box.component';
-import { finalize, take, tap } from 'rxjs';
+import { finalize, Subject, take, takeUntil, tap } from 'rxjs';
 import { TripsStore } from '../../stores/trips.store';
 import { TripsService } from '../../services/trips.service';
 
@@ -18,21 +18,23 @@ import { TripsService } from '../../services/trips.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  // #region Properties (2)
+export class HomeComponent implements OnInit {
+  // #region Properties (3)
+
+  private unsubscribe = new Subject<void>();
 
   public store = inject(TripsStore);
   public tripService = inject(TripsService);
 
-  // #endregion Properties (2)
+  // #endregion Properties (3)
 
-  // #region Public Methods (1)
+  // #region Public Methods (2)
 
   public getTrips(): void {
     this.store.updateLoading(true);
 
     this.tripService.getTrips().pipe(
-      take(1),
+      takeUntil(this.unsubscribe),
       finalize(() => this.store.updateLoading(false)),
       tap(data => {
         this.store.updateTrips(data.items.map(item => this.tripService.convertTripFromBeToFe(item)));
@@ -46,5 +48,9 @@ export class HomeComponent {
     ).subscribe();
   }
 
-  // #endregion Public Methods (1)
+  public ngOnInit(): void {
+    this.getTrips();
+  }
+
+  // #endregion Public Methods (2)
 }
